404423b4e686a0b51390e86336574be19853bd63
justbenuk <justbenuk@gmail.com>
2023-02-13 13:45:49 +0000

    added dev db and fixed admin url for production

--- config/database.js
+++ config/database.js
@@ -1,18 +1,33 @@
-const path = require('path');
+const path = require( 'path' );
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'app-ea10d659-4217-48f7-8ff6-e05e5048842e-do-user-13544363-0.b.db.ondigitalocean.com'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+module.exports = ( { env } ) => {
+  if ( env( 'NODE_ENV' ) === 'development' ) {
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join( __dirname, '..', env( 'DATABASE_FILENAME', '.tmp/data.db' ) ),
+        },
+        useNullAsDefault: true,
       },
-    },
-    debug: false,
-  },
-});
+    }
+  } else {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env( 'DATABASE_HOST', 'app-ea10d659-4217-48f7-8ff6-e05e5048842e-do-user-13544363-0.b.db.ondigitalocean.com' ),
+          port: env.int( 'DATABASE_PORT', 3306 ),
+          database: env( 'DATABASE_NAME', 'strapi' ),
+          user: env( 'DATABASE_USERNAME', 'strapi' ),
+          password: env( 'DATABASE_PASSWORD', 'strapi' ),
+          ssl: {
+            rejectUnauthorized: env.bool( 'DATABASE_SSL_SELF', false ), // For self-signed certificates
+          },
+        },
+        debug: false,
+      },
+    }
+
+  }
+};
--- config/server.js
+++ config/server.js
@@ -1,11 +1,26 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
- url: 'https://stingray-app-gukxi.ondigitalocean.app/cp/',
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ( { env } ) => {
+  if ( env( 'NODE_ENV' ) === 'development' ) {
+    return {
+      host: env( 'HOST', '0.0.0.0' ),
+      port: env.int( 'PORT', 1337 ),
+      app: {
+        keys: env.array( 'APP_KEYS' ),
+      },
+      webhooks: {
+        populateRelations: env.bool( 'WEBHOOKS_POPULATE_RELATIONS', false ),
+      },
+    }
+  } else {
+    return {
+      host: env( 'HOST', '0.0.0.0' ),
+      port: env.int( 'PORT', 1337 ),
+      url: 'https://justben.uk/cp/admin',
+      app: {
+        keys: env.array( 'APP_KEYS' ),
+      },
+      webhooks: {
+        populateRelations: env.bool( 'WEBHOOKS_POPULATE_RELATIONS', false ),
+      },
+    }
+  }
+};
